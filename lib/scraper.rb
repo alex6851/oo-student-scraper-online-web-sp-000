1c8440e32e1389ec6329a5d419d9199b8fc142f2
Alexander Baker <alex.baker685@gmail.com>
2019-04-30 02:42:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,10 +5,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     binding.pry
-    open-uri(index_url)
+    open(index_url)
+    {
+      :name => , :location => :profile_url },
+    }
   end
 
   def self.scrape_profile_page(profile_url)
+    
 
   end
 
