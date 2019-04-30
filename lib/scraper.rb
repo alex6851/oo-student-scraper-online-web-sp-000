c11f6a8af32ba52a93cd160cedd3c2b554e68085
Alexander Baker <alex.baker685@gmail.com>
2019-04-30 03:09:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,8 @@ class Scraper
     binding.pry
     # doc = Nokogiri::HTML((index_url))
     # binding.pry
-    open(index_url, :name => //div/a, :location => "location", :profile_url=>"students/name"){|f|
-    }
+    # open(index_url, :name => //div/a, :location => "location", :profile_url=>"students/name"){|f|
+    # }
   end
 
   def self.scrape_profile_page(profile_url)
