1981e06685c84b788b76bf2f9e986ce0f9a6b533
Alexander Baker <alex.baker685@gmail.com>
2019-04-30 02:32:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,13 @@ require 'pry'
 
 class Scraper
 
+  open-uri(index_url)
+
   def self.scrape_index_page(index_url)
     {:name => "Ryan Johnson", :location => "New York, NY", :profile_url =>"students/ryan-johnson.html" },
-    {:name =>"Eric Chu", :location =>"Glenelg, MD", :profile_url =>"students/eric-chu.html"},
-    {:name =>"Aaron Enser", :location =>"Scottsday, AZ", :profile_url =>"students/aaron-enser.html" },
-    {:name => , :location => , :profile_url => },
+    {:name => "Eric Chu", :location => "Glenelg, MD", :profile_url =>"students/eric-chu.html"},
+    {:name => "Aaron Enser", :location => "Scottsdale, AZ", :profile_url =>"students/aaron-enser.html" },
+    {:name => "Brigan Enser", :location => "Scottsdale, AZ", :profile_url =>"students/brigan-enser.html" },
   end
 
   def self.scrape_profile_page(profile_url)
